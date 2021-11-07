4741f16b0c18758d720d058f2d745fc2763f6b41
chabanovsky <nicolas@chabanovsky.com>
2021-11-07 19:13:26 +0300

    Added a way to split into buckets

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 
@@ -32,3 +33,52 @@ def date_moving_avarage(df, date_field, group_field, period=29, day_threshold=1)
         "OnDate": result_idx,
         "Value": result_values
     })     
+
+def display_buckets(buckets, freq, group_field='CrationDate', count_field='PostId'):
+    data = []
+    for index, bucket in enumerate(buckets):
+        data.append(
+            bucket['bucket'].groupby(pd.Grouper(key=group_field, freq=freq))[count_field].count().rename(
+                "Bucket %d, [%d; %d], total %d" % (
+                    index, bucket['low'], bucket['hight'], bucket['total']
+                )
+            )
+        )
+    return data
+    
+def split_into_buckets(df, groupby_field='CreationUserId', count_field='PostId', iterations=3, need_report=True):
+    def add_bucket(buckets, sliced, low, hight, index, need_report):
+        total = sliced.groupby([groupby_field])[count_field].count().size
+        if need_report:
+            print("Bucket %f [%d; %d], size %d" % (index, low, hight, total))
+
+        buckets.append({
+            "bucket": sliced,
+            "low": low,
+            "hight": hight,
+            "total": total
+        })
+        
+    if need_report:
+        print("Total size %s" % (str(df.groupby([groupby_field])[count_field].count().size)))
+        
+    buckets = []
+    threshold_low = 0
+    for index in range(iterations):
+        tmp_df = df.groupby([groupby_field])[count_field].count()
+        q75 = tmp_df.quantile(0.75)
+        q25 = tmp_df.quantile(0.25)
+        iqr = q75 - q25
+        threshold_hight = int(round(q75 + 1.5 * iqr))
+        tmp_df = tmp_df[(tmp_df >= threshold_low) & (tmp_df <= threshold_hight)]
+        
+        the_slice = df[df[groupby_field].isin(set(tmp_df.index))]
+        the_rest = df[~df[groupby_field].isin(set(tmp_df.index))]
+        add_bucket(buckets, the_slice, threshold_low, threshold_hight, index, need_report)
+        threshold_low = threshold_hight + 1
+        df = the_rest
+        if index+1 == iterations: # the last iteration
+            add_bucket(buckets, the_rest, threshold_low, -1, index+1, need_report)
+            
+        
+    return buckets
