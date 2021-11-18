f3f485c88bf6277a844c97efa91eea8594612f4a
chabanovsky <nicolas@chabanovsky.com>
2021-11-18 18:58:18 +0300

    Adjusted typing for hover in the plot_bokeh function

--- report.py
+++ report.py
@@ -125,7 +125,7 @@ def plot_bokeh_(df, title, xlabel, ylabel, stacked, need_table, location="top_le
     if is_datetime(df.index):
         hover = HoverTool(tooltips=tooltips, formatters={'@' + df.index.name: 'datetime'})
     else:
-        hover = HoverTool(tooltips=tooltips)
+        hover = HoverTool(tooltips=tooltips, formatters={'@' + df.index.name: 'numeral'})
     p.add_tools(hover)
 
     show(p)
